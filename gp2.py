bdb130b3b0cc6927e1f709312b6c7699bb95b7df
Nikolaj <nhin@912134.xyz>
2023-01-16 17:00:01 +0100

    bayesian optimisation plot

--- gp2.py
+++ gp2.py
@@ -5,7 +5,7 @@ import pyro.contrib.gp as gp
 import pyro.distributions as dist
 import arviz
 
-from util import gp_plot
+from util import bayesian_optimisation_plot
 
 def f(x):
   assert (x >= -1).all() and (x <= 1).all(), f"f({x}) is not in domain of f"
@@ -26,7 +26,7 @@ def sample_posterior(model, S=1, C=1, W=1000):
   return {k[len("kernel."):]: v[-1] for k, v in mcmc.get_samples().items()}
 
 # Bayesian optimisation
-def bayesian_optimisation(kernel, kernel_priors, D, xstar, T):
+def bayesian_optimisation(kernel, kernel_priors, D, xstar, T, saveT=()):
   # kernel is a function that returns the kernel when given args.
   # kernel_priors is the kernel instantiated with priors as args.
   # D is the initial dataset {(x_1, f(x_1)), ..., (x_n, f(x_n))}.
@@ -36,39 +36,52 @@ def bayesian_optimisation(kernel, kernel_priors, D, xstar, T):
   # Allocate dataset arrays to hold iteration results.
   x, y = torch.empty(n+T), torch.empty(n+T)
   x[:n], y[:n] = D
-  # Iteratively infer hyperparameters given D, predict best local minimum candidate
-  # for evaluation and update D.
+  means, vars = torch.empty(T+1, len(xstar)), torch.empty(T+1, len(xstar))
+  # Iteratively infer hyperparameters given D, predict best local minimum
+  # candidate for evaluation and update D.
   kernel = partial(kernel, input_dim=1)
-  for k in range(n,n+T):
-    # Sample kernel hyperparameters from the posterior given current dataset D.
+  min = torch.min(y)
+  for k in range(0,T):
+    # Init GP on current D with prior hyperparameters.
     pyro.clear_param_store()
     gpr = gp.models.GPRegression(
-      x[:k], y[:k], kernel_prior, noise=torch.tensor(1e-4)
+      x[:n+k], y[:n+k], kernel_prior, noise=torch.tensor(1e-4)
     )
+    # Save parameters for plotting at k=0 (which is k=-1 here).
+    if k == 0:
+      means[k], vars[k] = gpr(xstar, full_cov=False, noiseless=False)
+    # Sample kernel hyperparameters from the posterior.
     theta = sample_posterior(gpr.model)
-    print(theta)
     gpr.kernel = kernel(**theta)
+    print(theta)
 
+    # Algorithm 1
     # Sample f* ~ p(f*|X*,D).
     mean, var = gpr(xstar, full_cov=False, noiseless=False)
     fstar = dist.Normal(mean, var.sqrt()).rsample()
-    # Find local minimum from predictions (sampled f*).
+    # Find local minimum of predictions (sampled function f*).
     p = torch.argmin(fstar)
     # Add (x^*_p, f(x^*_p)) to the dataset D.
-    x[k], y[k] = xstar[p], f(xstar[p])
-  return (x[n:], y[n:]), mean.detach(), var.detach()
+    x[n+k], y[n+k] = xstar[p], f(xstar[p])
+
+    # Save parameters for plotting
+    means[k+1], vars[k+1] = mean, var
+  return torch.min(y), ((x[n:], y[n:]), means.detach(), vars.detach())
 
 # Need to instantiate kernel with priors here since the RBF (Isotropy)
 # class wraps init arguments in PyroParam and we don't want that.
 kernel_prior = gp.kernels.RBF(input_dim=1)
-# note use of _log_normal: priors have support on the positive reals
 kernel_prior.lengthscale = pyro.nn.PyroSample(dist.LogNormal(-1.0, 1.0))
 kernel_prior.variance = pyro.nn.PyroSample(dist.LogNormal(0.0, 2.0))
 
-(xp, yp), mean, var = bayesian_optimisation(gp.kernels.RBF, kernel_prior, (x, y), xstar, 5)
-print(x, y)
-gp_plot(f, x, y, xstar, mean, var, filename="bayesian_optimisation.pdf",
-  xnew=xp, ynew=yp,
+ymin, ((xp, yp), means, vars) = bayesian_optimisation(gp.kernels.RBF, kernel_prior, (x, y), xstar, 10)
+
+print("x*_p", xp)
+print("y*_p", yp)
+print("ymin", ymin)
+
+bayesian_optimisation_plot(f, x, y, xstar, means, vars,
+  xp, yp, [0,5,10],
 )
 
 # From anaylising plots from multiple reruns it seems that a single observed
--- util.py
+++ util.py
@@ -5,7 +5,7 @@ import numpy
 matplotlib.rcParams.update({'font.size': 18})
 
 # Modified from https://pyro.ai/examples/gp.html.
-def gp_plot(f, x, y, xstar, mean, var, filename="gp_plot.pdf", xnew=None, ynew=None):
+def gp_plot(f, x, y, xstar, mean, var, filename="gp_plot.pdf"):
   fig, ax = plt.subplots(figsize=(12, 6))
 
   # Visualize p(f*|x*,D).
@@ -24,15 +24,47 @@ def gp_plot(f, x, y, xstar, mean, var, filename="gp_plot.pdf", xnew=None, ynew=N
   ax.plot(xstar.numpy(), f(xstar).numpy(), "k", lw=2, label="$f(x)$")
   # Scatter plot D.
   ax.plot(x.numpy(), y.numpy(), "kx", markersize=15, lw=4, label="$\mathcal{D}$")
-  # Scatter plot (x*, f(x*)) added to D.
-  if xnew is not None:
-    assert ynew is not None
-    ax.plot(xnew.numpy(), ynew.numpy(), "k*", markersize=16, label="$(x_p^*, f(x_p^*))$")
 
-  plt.xlabel("x")
-  plt.ylabel("y")
+  plt.xlabel("$x$")
+  plt.ylabel("$y$")
 
   plt.legend()
+  plt.tight_layout()
+  plt.savefig(filename)
+
+def bayesian_optimisation_plot(f, x, y, xstar, means, vars, xp, yp, ks, filename="bayesian_optimisation.pdf"):
+  fig, axs = plt.subplots(len(ks), figsize=(12, 12))
+
+  ymin = min(y)
+  for ax, k in zip(axs, ks):
+    mean, var = means[k], vars[k]
+    xnew, ynew = xp[:k], yp[:k]
+    ymin = min(ymin, min(ynew)) if len(ynew) > 0 else ymin
+    # Visualize p(f*|x*,D).
+    sd = var.sqrt()  # standard deviation at each input point x
+    ax.plot(xstar.numpy(), mean.numpy(), "m", lw=3, ls="--", label="$m(x^*)$")
+    ax.fill_between(
+        xstar.numpy(),  # plot the two-sigma uncertainty about the mean
+        (mean - 2.0 * sd).numpy(),
+        (mean + 2.0 * sd).numpy(),
+        color="C0",
+        alpha=0.3,
+        label="$m(x^*) \pm 2\sqrt{v(x^*)}$",
+    )
+
+    # Plot f(x*).
+    ax.plot(xstar.numpy(), f(xstar).numpy(), "k", lw=2, label="$f(x)$")
+    # Scatter plot D.
+    ax.plot(x.numpy(), y.numpy(), "kx", markersize=15, lw=4, label="$\mathcal{D}$")
+    # Scatter plot (x*, f(x*)) added to D.
+    if xnew is not None:
+      assert ynew is not None
+      ax.plot(xnew.numpy(), ynew.numpy(), "k*", markersize=16, label="$(x_p^*, f(x_p^*))$")
+      ax.set_title("$k=$" + f"{k}" + ",   $y_{min}=$" + f"{ymin:.2f}")
+      ax.set_ylabel("$y$")
+
+  axs[-1].set_xlabel("$x$")
+  axs[0].legend(ncol=5, bbox_to_anchor=(0, 1.1, 1, 0), loc="lower left", mode="expand")
   plt.tight_layout()
   plt.savefig(filename)
 
